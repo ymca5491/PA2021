4621a09989d963d0782d8b5b7c2c22fb483d0999
tracer-ics2021 <tracer@njuics.org>
2021-09-25 22:21:51 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 9ce3b368b5084ffca5f492f6c5f2be5d   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  22:21:51 up 15:33,  3 users,  load average: 0.49, 0.76, 0.50

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -3,7 +3,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
-#include <utils.h>
 
 static int is_batch_mode = false;
 
@@ -111,7 +110,7 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { nemu_state.state = NEMU_QUIT; return; }
+        if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
     }
