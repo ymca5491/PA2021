c6deab4d58220ed0f83c0ea1eacaa918366ae0cf
tracer-ics2021 <tracer@njuics.org>
2021-09-28 16:45:52 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 5e729dca04f34920a70c97d7e1bd3a67   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  16:45:52 up  1:00,  3 users,  load average: 1.79, 1.08, 0.73

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -17,7 +17,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   for (int i = 0; i < 32; i++) {
-    if (regs[i] == s) {
+    if (strcmp(regs[i], s) == 0) {
       *success = true;
       return gpr(i);
     }
