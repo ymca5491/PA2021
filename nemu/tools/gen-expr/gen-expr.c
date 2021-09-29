b1e6b89b952a1baeb2b25d30e0e4d62239f19c1d
tracer-ics2021 <tracer@njuics.org>
2021-09-29 21:26:14 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 43bb1410bc1046f6972a50925ce07366   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  21:26:14 up  3:31,  3 users,  load average: 0.03, 0.11, 0.24

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -61,7 +61,10 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr(true);
-
+    if(strlen(buf) >= 31) {
+      i--;
+      continue;
+    }
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
