865c21f68ccbfae356eee311f962bd69e90c57bd
tracer-ics2021 <tracer@njuics.org>
2021-09-29 20:51:56 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 43bb1410bc1046f6972a50925ce07366   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  20:51:56 up  2:57,  3 users,  load average: 0.14, 0.18, 0.20

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -97,7 +97,7 @@ static bool make_token(char *e) {
             nr_token++;
             break;
           case TK_DEC: case TK_HEX: case TK_REG:
-            if (substr_len > 31) {puts("Too long numbers not supported"); break;}
+            if (substr_len > 31) {puts("Too long numbers not supported"); return false;}
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token].str, substr_start, substr_len);
             tokens[nr_token].str[substr_len] = '\0';
