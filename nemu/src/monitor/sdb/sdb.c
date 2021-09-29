0761b98493bfadc9647ed719095fe65f63538f2e
tracer-ics2021 <tracer@njuics.org>
2021-09-29 20:35:35 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 43bb1410bc1046f6972a50925ce07366   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  20:35:35 up  2:40,  3 users,  load average: 0.30, 0.18, 0.24

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -204,7 +204,7 @@ static int cmd_exprtest(char* args) {
     char* t_expr = strtok(NULL, " ");
     bool success;
 
-    printf("%s\n%s", t_res_s, t_expr);
+    //printf("%s\n%s", t_res_s, t_expr);
 
     word_t res = expr(t_expr, &success);
     word_t t_res;
