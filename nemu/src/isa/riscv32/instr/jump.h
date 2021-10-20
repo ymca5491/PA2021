c624eb4bc452fbcf0042ec940f1c8addd17dbac1
tracer-ics2021 <tracer@njuics.org>
2021-10-20 20:25:46 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 1426cc3fa5b749aba78b6b649ff78132   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:25:46 up  4:12,  3 users,  load average: 0.69, 0.72, 0.74

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -1,4 +1,13 @@
 def_EHelper(jal) {
   rtl_li(s, ddest, s->snpc);
-  rtl_j(s, s->pc + id_src1->simm);
+  rtl_addi(s, s0, &s->pc, id_src1->simm);
+  rtl_jr(s, s0);
+}
+
+def_EHelper(jalr) {
+  rtl_li(s, ddest, s->snpc);
+  rtl_addi(s, s0, dsrc1, id_src2->simm);
+  rtl_srli(s, s0, s0, 1);
+  rtl_slli(s, s0, s0, 1);
+  rtl_jr(s, s0);
 }
