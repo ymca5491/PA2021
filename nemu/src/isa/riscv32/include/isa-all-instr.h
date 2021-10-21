5032d0a00ba97c59002039913d5f8dcc72f12327
tracer-ics2021 <tracer@njuics.org>
2021-10-21 20:48:38 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: f54b9dbd632c4d56b5542763c64fc792   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:48:38 up  3:00,  4 users,  load average: 0.58, 0.67, 0.66

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,7 +2,7 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(add) f(sub) f(and) f(or) f(xor) f(sll) f(sltu)\
-                      f(addi) f(andi) f(sltiu) f(srai)\
+                      f(addi) f(andi) f(xori) f(sltiu) f(srai)\
                       f(lui) f(auipc) \
                       f(jal) f(jalr) \
                       f(lw) f(lbu) f(sw) f(sh)\
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -15,6 +15,10 @@ def_EHelper(andi) {
   rtl_addi(s, ddest, dsrc1, id_src2->simm);
 }
 
+def_EHelper(xori) {
+  rtl_xori(s, ddest, dsrc1, id_src2->simm);
+}
+
 def_EHelper(sltiu) {
   rtl_setrelopi(s, RELOP_LTU, ddest, dsrc1, id_src2->imm);
 }
