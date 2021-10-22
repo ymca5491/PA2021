15f68068857a4a9c235279a3c0b5fce100c26fab
tracer-ics2021 <tracer@njuics.org>
2021-10-22 21:57:12 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 557df6a9e91549768fec233e04baf566   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:57:12 up  2:00,  3 users,  load average: 1.00, 0.84, 0.76

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,137 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t c = 0;
+  while (*s) {
+    s++;
+    c++;
+  }
+  return c;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dst_cp = dst;
+  while (*src) {
+    *dst_cp = *src;
+    dst_cp++;
+    src++;
+  }
+  *dst_cp = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *dst_cp = dst;
+  for (size_t i = 0; i < n; i++) {
+    if (*src) {
+      *dst_cp = *src;
+      dst_cp++;
+      src++;
+    }
+    else {
+      *dst_cp = '\0';
+      dst_cp++;
+    }
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dst_tail = dst + strlen(dst);
+  while (*src) {
+    *dst_tail = *src;
+    dst_tail++;
+    src++;
+  }
+  *dst_tail = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while(*s1 || *s2) {
+    if (*s1 < *s2) {
+      return -1;
+    }
+    else if (*s1 > *s2){
+      return 1;
+    }
+    s1++;
+    s2++;
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++) {
+    if (*s1 < *s2) {
+      return -1;
+    }
+    else if (*s1 > *s2){
+      return 1;
+    }
+    else if (!*s1 && !*s2) {
+      return 0;
+    }
+    s1++;
+    s2++;
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  uint8_t* t = s;
+  for (size_t i = 0; i < n; i++) {
+    *t = c;
+    t++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  uint8_t* pDst = dst;
+  const uint8_t* pSrc = src;
+  uint8_t* t = malloc(n * sizeof(uint8_t));
+  uint8_t* pT = t;
+  size_t i;
+  for (i = 0; i < n; i++) {
+    *pT = *pSrc;
+    pT++;
+    pSrc++;
+  }
+  for (i = 0; i < n; i++) {
+    *pDst = *pT;
+    pT++;
+    pDst++;
+  }
+  free(t);
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  uint8_t* pDst = out;
+  const uint8_t* pSrc = in;
+  for (size_t i = 0; i < n; i++) {
+    *pDst = *pSrc;
+    pDst++;
+    pSrc++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const uint8_t* p1 = s1;
+  const uint8_t* p2 = s2;
+  for (size_t i = 0; i < n; i++) {
+    if (*p1 < *p2) {
+      return -1;
+    }
+    else if (*p1 > *p2){
+      return 1;
+    }
+    p1++;
+    p2++;
+  }
+  return 0;
 }
 
 #endif
