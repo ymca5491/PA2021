19789bc239cea5fa7a5138140f151724be00028a
tracer-ics2021 <tracer@njuics.org>
2021-11-24 00:59:28 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 85a15c7a7b324ca6860e107f138bb8a6   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:59:28 up 3 min,  3 users,  load average: 0.58, 0.61, 0.28

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -108,6 +108,8 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     is_skip_ref = false;
     return;
   }
+  ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+  printf("0x%x", ref_r.pc);
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
