0604271383773d9c3cf3a623f537e6229b75e74a
tracer-ics2021 <tracer@njuics.org>
2021-11-23 22:01:50 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 04754fdb18b147698f0100228698dcbb   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:01:50 up  2:14,  3 users,  load average: 0.13, 0.16, 0.26

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -6,9 +6,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
 
   /* mstatus 0x300, mtvec 0x305, mepc 0x341, mcause 0x342 */
-  gpr(0x341) = epc << 2;
+  gpr(0x341) = epc;
   gpr(0x342) = NO;
-  return gpr(0x305);
+  return gpr(0x305) & 0xfc;
 }
 
 word_t isa_query_intr() {
