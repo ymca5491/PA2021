c43d535d06c2fe54d4a5bc7c51fc123abd7640a9
tracer-ics2021 <tracer@njuics.org>
2021-11-23 22:17:39 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 04754fdb18b147698f0100228698dcbb   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:17:39 up  2:30,  3 users,  load average: 0.43, 0.29, 0.27

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -1,5 +1,5 @@
 #include <isa.h>
-#include <../local-include/reg.h>
+#include "../local-include/reg.h"
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
@@ -8,7 +8,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* mstatus 0x300, mtvec 0x305, mepc 0x341, mcause 0x342 */
   gpr(0x341) = epc;
   gpr(0x342) = NO;
-  return gpr(0x305) & 0xfffffffc;
+  return gpr(0x305);
 }
 
 word_t isa_query_intr() {
