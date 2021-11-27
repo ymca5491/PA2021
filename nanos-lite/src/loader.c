22d054f9f00633889b165d2f680d26c366fad842
tracer-ics2021 <tracer@njuics.org>
2021-11-27 19:08:10 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 1f7ee249b1044db5b53626a8039618e6   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:08:10 up  1:34,  3 users,  load average: 0.16, 0.29, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -10,7 +11,19 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  //TODO();
+  Elf_Ehdr head;
+  Elf_Phdr phdr;
+  ramdisk_read(&head, 0, sizeof(Elf_Ehdr));
+  for (int i = 0; i < head.e_phnum; i++) {
+    ramdisk_read(&phdr, head.e_phoff + i*head.e_phentsize, head.e_phentsize);
+    if (phdr.p_type == PT_LOAD) {
+      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+      for (char *p = (char *)phdr.p_vaddr + phdr.p_filesz; p != (char *)phdr.p_vaddr + phdr.p_memsz; p++) {
+        *p = 0;
+      }
+    }
+  }
   return 0;
 }
 
