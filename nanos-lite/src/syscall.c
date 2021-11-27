2da6379c4c5b4a7da9f5a63651af819361c92e9e
tracer-ics2021 <tracer@njuics.org>
2021-11-28 01:04:56 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 550430d28860487993753787b597ebd7   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  01:04:56 up  1:52,  4 users,  load average: 0.38, 0.36, 0.38

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,6 @@
 #include <common.h>
 #include "syscall.h"
-//#define ETRACE
+#define STRACE
 
 uintptr_t sys_write(int fd, const void* buf, size_t n);
 
@@ -11,16 +11,17 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-#ifdef ETRACE
+#ifdef STRACE
   printf("Syscall type:%d, args: %d, %d, %d\n", a[0], a[1], a[2], a[3]);
 #endif
   switch (a[0]) {
     case SYS_yield: yield(); ret = 0; break;
-    case SYS_exit: halt(a[1]); break;
+    case SYS_exit:  halt(a[1]); break;
     case SYS_write: ret = sys_write(a[1], (void *)a[2], a[3]); break;
+    case SYS_brk:   
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-#ifdef ETRACE
+#ifdef STRACE
   printf("Syscall return value: %d\n", ret);
 #endif
   c->GPRx = ret;
