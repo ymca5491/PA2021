6d632c99715b59397244b84c8b36eabe1da6da90
tracer-ics2021 <tracer@njuics.org>
2021-11-27 23:30:12 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 550430d28860487993753787b597ebd7   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:30:12 up 17 min,  3 users,  load average: 0.47, 0.55, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,7 +7,7 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-  printf("type:%u", a[0]);
+  printf("type:%d\n", a[0]);
   switch (a[0]) {
     case SYS_yield: yield(); c->mepc += 4; c->GPRx = 0; break;
 
