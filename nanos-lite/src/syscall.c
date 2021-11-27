d3299bcd0e323b03bc0f0487aded0c0efa9d2e72
tracer-ics2021 <tracer@njuics.org>
2021-11-28 00:18:34 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 550430d28860487993753787b597ebd7   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:18:34 up  1:05,  4 users,  load average: 0.41, 0.35, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,8 @@
 #include <common.h>
 #include "syscall.h"
+
+uintptr_t sys_write(int fd, const void* buf, size_t n);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   uintptr_t ret = 0;
@@ -12,8 +15,20 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield: yield(); ret = 0; break;
     case SYS_exit: halt(a[1]); break;
+    case SYS_write: ret = sys_write(a[1], (void *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   printf("syscall return value: %d\n", ret);
   c->GPRx = ret;
+}
+
+uintptr_t sys_write(int fd, const void* buf, size_t n) {
+  switch (fd) {
+    case 1: case 2:
+      for (int i = 0; i < n; i++) putch(*((char*)(buf) + i));
+      return n;
+      break;
+    
+    default: panic("Not implemented");
+  }
 }
