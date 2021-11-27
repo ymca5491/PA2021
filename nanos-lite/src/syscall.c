a255b10d1954ecb16c7375a254f5fb24d2770f43
tracer-ics2021 <tracer@njuics.org>
2021-11-27 23:57:22 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 550430d28860487993753787b597ebd7   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:57:22 up 44 min,  4 users,  load average: 0.54, 0.47, 0.38

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,15 +2,18 @@
 #include "syscall.h"
 void do_syscall(Context *c) {
   uintptr_t a[4];
+  uintptr_t ret = 0;
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-  //printf("type:%d\n", a[0]);
+  printf("syscall type:%d\n, args =: %d, %d, %d", a[0], a[1], a[2], a[3]);
   switch (a[0]) {
-    case SYS_yield: yield(); c->GPRx = 0; break;
+    case SYS_yield: yield(); ret = 0; break;
     case SYS_exit: halt(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  printf("syscall return value: %d", ret);
+  c->GPRx = ret;
 }
