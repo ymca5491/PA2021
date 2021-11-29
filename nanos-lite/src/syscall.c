11816fa4118bdb47953ddc267e186ffd13c6e758
tracer-ics2021 <tracer@njuics.org>
2021-11-30 01:50:42 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 4981b0843aa7467bad7fb99bc07b39a6   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  01:50:42 up  3:11,  4 users,  load average: 0.57, 0.54, 0.43

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,10 @@
 #include <common.h>
 #include "syscall.h"
 #include <fs.h>
+//#include <nemu.h>
+//#include <riscv.h>
+
+//uintptr_t sys_gettimeofday(struct timeval *tv, struct timezone *tz);
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -21,6 +25,7 @@ void do_syscall(Context *c) {
     case SYS_write: ret = fs_write(a[1], (void *)a[2], a[3]); break;
     case SYS_close: ret = fs_close(a[1]); break;
     case SYS_brk:   ret = 0; break;
+    //case SYS_gettimeofday: ret = sys_gettimeofday((struct timeval *)a[1], (struct timezone *)a[2]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 #ifdef STRACE
@@ -28,3 +33,14 @@ void do_syscall(Context *c) {
 #endif
   c->GPRx = ret;
 }
+/*
+uintptr_t sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
+  long useconds;
+  useconds = inl(RTC_ADDR + 4);
+  useconds <<= 32;
+  useconds += inl(RTC_ADDR);
+  tv->tv_sec = useconds / 1000000;
+  tv->tv_usec = useconds - 1000000 * tv->tv_sec;
+  return 0;
+}
+*/
---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,19 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+    struct timeval t;
+    struct timeval new;
+    int n = 0;
+    assert(gettimeofday(&t, NULL) == 0);
+    while (1) {
+        assert(gettimeofday(&new, NULL) == 0);
+        int update = ((new.tv_sec - t.tv_sec) * 1000000 + (new.tv_usec - t.tv_usec)) / 500000;
+        for (int i = 0; i < update; i++) {
+            n++;
+            printf("%d.%ds passed\n", n / 2, (n % 2) ? 5 : 0);
+        }
+    }
+    return 0;
+}
