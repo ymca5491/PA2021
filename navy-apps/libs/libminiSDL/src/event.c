601a3d53d2829443444a6bd9f56267b0b4027ac0
tracer-ics2021 <tracer@njuics.org>
2021-12-04 02:30:20 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 79a4a38aa9d34171988b210b6ba7bf5f   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  02:30:20 up  2:48,  4 users,  load average: 0.67, 0.53, 0.56

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,7 +18,21 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64];
+  char kstate[2];
+  char kname[32];
+  while(1) {
+    if (event->type != SDL_USEREVENT) {
+      NDL_PollEvent(buf, sizeof(buf));
+      sscanf(buf, "%s %s\n", kstate, kname);
+      if (strcmp(kstate, "kd") == 0 && event->type == SDL_KEYDOWN) {
+        if (strcmp(kname, keyname[event->key.keysym.sym]) == 0 ) return 1;
+      }
+      else if (strcmp(kstate, "ku") == 0 && event->type == SDL_KEYUP) {
+        if (strcmp(kname, keyname[event->key.keysym.sym]) == 0 ) return 1;
+      }
+    }
+  }
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
