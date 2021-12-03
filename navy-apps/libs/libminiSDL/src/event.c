fc51c11fd6e5d50fdf2824f24a9e41f7ca32e4f2
tracer-ics2021 <tracer@njuics.org>
2021-12-04 02:52:17 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 79a4a38aa9d34171988b210b6ba7bf5f   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  02:52:17 up  3:09,  4 users,  load average: 1.06, 0.75, 0.64

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -26,8 +26,8 @@ int SDL_WaitEvent(SDL_Event *event) {
       event->type = SDL_KEYDOWN;
     else 
       event->type = SDL_KEYUP;
-    sscanf(&(buf[3]), "%skey\n", kn);
-    printf("%s\n", kn);
+    sscanf(&(buf[3]), "%s\n", kn);
+    printf("%skey\n", kn);
     for (int i = 0; i < sizeof(keyname); i++) {
       if (strcmp(kn , keyname[i]) == 0) {
         event->key.keysym.sym = i;
