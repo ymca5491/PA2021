f82bb5a077e52bf1f6026648e7147f6fa3288115
tracer-ics2021 <tracer@njuics.org>
2021-12-03 16:09:29 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: f4393ec6558b40618b4fdf3bbb9c9916   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:09:29 up 10 min,  3 users,  load average: 0.83, 1.06, 0.65

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -74,7 +74,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   y += (screen_h - canvas_h) / 2;
   for (int j = y; j < y + h; j++) {
     lseek(fd, j * screen_w + x, SEEK_SET);
-    write(fd, pixels, 4 * w);
+    write(fd, pixels,  4 * w);
     pixels += w;
   }
   close(fd);
