4ed84c4794981751e55d92edb4ecf800503be1ea
tracer-ics2021 <tracer@njuics.org>
2021-12-04 19:59:21 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 5c44a4488a2c470cac0ecc6718ff48c4   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:59:21 up  1:02,  3 users,  load average: 0.95, 0.79, 0.60

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -44,7 +44,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T sz = io_read(AM_GPU_CONFIG);
   int y = offset / (4 * sz.width);
   int x = (offset / 4) % sz.width;
-  io_write(AM_GPU_FBDRAW, .x = x, .y = y, .pixels = buf, .w = len / 4, .h = 1, .sync = 1);
+  io_write(AM_GPU_FBDRAW, .x = x, .y = y, .pixels = (void*)buf, .w = len / 4, .h = 1, .sync = 1);
   return len;
 }
 
