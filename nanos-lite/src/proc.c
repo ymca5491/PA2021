5805b792afb368fb6aa09a4600f2bbbbe51ab518
tracer-ics2021 <tracer@njuics.org>
2022-01-13 05:31:54 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 2f78f2f6cf084a198eaa8ae98a0b4cff   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  05:31:54 up  5:34,  4 users,  load average: 0.19, 0.43, 0.57

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -45,7 +45,7 @@ void init_proc() {
 Context* schedule(Context *prev) {
   // save the context pointer
   current->cp = prev;
-
+  printf("pcb[0] at 0x%p", &pcb[0]);
   // always select pcb[0] as the new process
   current = &pcb[0];
   //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
