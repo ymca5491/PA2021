16a7af69bd0eb0b5bb24c3acf323ace1d6aea6cf
tracer-ics2021 <tracer@njuics.org>
2022-01-13 05:23:51 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 2f78f2f6cf084a198eaa8ae98a0b4cff   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  05:23:51 up  5:26,  4 users,  load average: 0.52, 0.70, 0.70

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -68,9 +68,9 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   //printf("Pass argv\n");
 
   for (int i = 0; envp[i] != NULL; i++) {
-    //printf("envp[%d] = %s", i, envp[i]);
+    printf("envp[%d] = %s", i, envp[i]);
     size = strlen(envp[i]) + 1;
-    st_top -= 0;
+    st_top -= size;
     buf[c++] = memcpy(st_top, envp[i], size);
   }
   buf[c++] = NULL;
