32788e93bb5c24f381e306a75614e16d4973d60a
tracer-ics2021 <tracer@njuics.org>
2022-01-15 03:25:37 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 3701630f1e2245d09bab0e101013a1e1   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  03:25:37 up  5:10,  4 users,  load average: 0.61, 0.54, 0.38

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,7 +7,7 @@ static Context* do_event(Event e, Context* c) {
     case EVENT_YIELD: c->mepc += 4; return schedule(c);
     case EVENT_SYSCALL: do_syscall(c); c->mepc += 4; break;
     case EVENT_IRQ_TIMER: 
-      Log("Reiceive EVENT_IRQ_TIMER");
+      //Log("Reiceive EVENT_IRQ_TIMER");
       return schedule(c); // irq_time comes after updatepc
     default: panic("Unhandled event ID = %d", e.event);
   }
