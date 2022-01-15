bd69975a1c16e91e8f4dfd94ce1a380d5b5753ef
tracer-ics2021 <tracer@njuics.org>
2022-01-16 06:07:02 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: cefea28f6e784bf8b3d5933e84ab6feb   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  06:07:02 up  6:36,  3 users,  load average: 1.27, 1.07, 0.84

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  //yield();
   const char* s = buf;
   for (int i = 0; i < len; i++) {
     putch(*s);
@@ -43,7 +43,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  //yield();
   AM_GPU_CONFIG_T sz = io_read(AM_GPU_CONFIG);
   int y = offset / (4 * sz.width);
   int x = (offset / 4) % sz.width;
