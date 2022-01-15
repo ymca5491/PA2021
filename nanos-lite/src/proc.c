284ef34e3ddf42cefd6de19a1e2f407745af822c
tracer-ics2021 <tracer@njuics.org>
2022-01-16 05:07:05 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: cefea28f6e784bf8b3d5933e84ab6feb   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  05:07:05 up  5:36,  3 users,  load average: 0.54, 0.58, 0.46

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,8 +32,9 @@ void init_proc() {
 
   char *empty[] = {NULL};
   char *argv_pal[] = {"/bin/pal","--skip", NULL};
-  context_uload(&pcb[0], "/bin/hello", NULL, NULL);
-  //context_uload(&pcb[1], "/bin/nterm", empty, empty);
+  int argk = -1;
+  context_kload(&pcb[0], hello_fun, &argk);
+  //context_uload(&pcb[0], "/bin/hello", NULL, NULL);
   context_uload(&pcb[1], argv_pal[0], argv_pal, empty);
   context_uload(&pcb[2], "/bin/bird", NULL, NULL);
   context_uload(&pcb[3], "/bin/nterm", NULL, NULL);
