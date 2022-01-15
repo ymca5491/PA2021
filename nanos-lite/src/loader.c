d5824aa0178d118518c5d9efc037cc45dd86d2e8
tracer-ics2021 <tracer@njuics.org>
2022-01-16 05:03:35 +0800

    >  compile 201300013    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: cefea28f6e784bf8b3d5933e84ab6feb   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  05:03:35 up  5:32,  3 users,  load average: 0.91, 0.48, 0.40

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,7 +25,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if (phdr.p_type == PT_LOAD) {
       pcb->max_brk = pcb->max_brk > phdr.p_paddr + phdr.p_memsz - 1 ? pcb->max_brk : phdr.p_paddr + phdr.p_memsz - 1;
       assert(fs_lseek(fd, phdr.p_offset, SEEK_SET) >= 0);
-      printf("load paddr = 0x%x, vaddr = 0x%x, memsz = 0x%x\n", phdr.p_paddr, phdr.p_vaddr, phdr.p_memsz);
+      //printf("load paddr = 0x%x, vaddr = 0x%x, memsz = 0x%x\n", phdr.p_paddr, phdr.p_vaddr, phdr.p_memsz);
       // sections are aligned with page size
       nrpg = ((phdr.p_paddr + phdr.p_memsz - 1) >> 12) - (phdr.p_paddr >> 12) + 1;
       vpg = phdr.p_paddr & 0xfffff000;
